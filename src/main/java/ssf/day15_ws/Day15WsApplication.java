58251b27da655fb364e591d3765a81f080b23d5b
Beatrice <beatricesim31@gmail.com>
2024-11-23 21:47:46 +0800

    springboot template

---
+++ src/main/java/ssf/day15_ws/Day15WsApplication.java
@@ -0,0 +1,13 @@
+package ssf.day15_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day15WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day15WsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/day15_ws/Day15WsApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day15_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day15WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
